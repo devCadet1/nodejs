3f01c98aed57bb5d24d35971d6884aa2fb01deb2
devCadet <satyam.karn3629@gmail.com>
2023-08-13 21:35:38 +0530

    practice

--- first.js
+++ first.js
@@ -1,9 +1,24 @@
+// var http = require('http');
+
+// http.createServer(function (req ,res) {
+//     res.writeHead(200, {
+//         'Content-Type' : 'text/html'
+//     })
+//     var bodyText = 'Hello DevCadet.'
+//     res.end (bodyText);
+// }).listen(8080)
+
+
+
+
 var http = require('http');
 
-http.createServer(function (req ,res) {
-    res.writeHead(200, {
-        'Content-Type' : 'text/html'
+var result = 'Learning NodeJS'
+
+http.createServer(function(req, res) {
+    res.writeHead(200 , {
+        'Content-Type' : 'Text'
     })
-    var bodyText = 'Hello DevCadet.'
-    res.end (bodyText);
+    
+    res.end(result)
 }).listen(8080)
